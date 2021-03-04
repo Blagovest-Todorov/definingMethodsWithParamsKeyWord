1d598874a2fa53d66d74c2442cd66c93ab752638
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-03-04 08:26:44 +0200

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace moreTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            decimal[] nums = new decimal[] { 10, 2, 7 };
+            PrintTotalAmountBooks(nums);
+        }
+
+        // keyword params into () allow us to call a mehtod with variable number of arguments
+        static void PrintTotalAmountBooks(params decimal[] prices)
+        {
+            decimal totalAmount = 0;
+
+            foreach (int singleBookPrice in prices)
+            {
+                totalAmount += singleBookPrice;
+            }
+
+            Console.WriteLine("the Total amount of all books is: " + totalAmount);
+        }        
+    }    
+}
